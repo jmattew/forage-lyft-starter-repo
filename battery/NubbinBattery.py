db4bfb7f788036cfd1c17daac52c544535bec3e9
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-08 00:25:49 -0700

    Create NubbinBattery.py

---
+++ battery/NubbinBattery.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+from car import Battery
+
+from datetime import datetime 
+
+
+class NubbinBattery(Battery, ABC): 
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date()
+            return True
+        else:
+            return False
