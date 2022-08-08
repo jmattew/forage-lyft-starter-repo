12ea1af0811a9f932ef61ad7962e5f0253692cdd
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-08 00:23:27 -0700

    Create SpindlerBattery.py

---
+++ battery/SpindlerBattery.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from car import Battery
+
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date()
+            return True
+        else:
+            return False
