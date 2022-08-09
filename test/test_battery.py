fe08dd28753b160ec41317d6e264fe3f2892cb70
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-09 10:40:07 -0700

    Update test_battery.py

--- test/test_battery.py
+++ test/test_battery.py
@@ -1,1 +1,35 @@
+import unittest
+from datetime import datetime
 
+from battery.SpindlerBattery import SpindlerBattery
+from battery.NubbinBattery import NubbinBattery
+
+class TestSpindler(unittest.TestCase)
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        spBattery = SpindlerBattery(self,today,last_service_date)
+        self.assertTrue(spBattery.needs_service())
+    
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        spBattery = SpindlerBattery(self,today,last_service_date)
+        self.assertFalse(spBattery.needs_service())
+    
+class TestNubbin(unittest.TestCase)
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+
+        nbBattery = NubbinBattery(last_service_date)
+        self.assertTrue(nbBattery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        nbBattery = NubbinBattery(last_service_date)
+        self.assertFalse(nbBattery.needs_service())
