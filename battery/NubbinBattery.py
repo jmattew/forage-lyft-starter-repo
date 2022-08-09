f2fff992724bb3590bb3bd5506853ebfc708b367
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-08 20:36:18 -0700

    Update NubbinBattery.py

--- battery/NubbinBattery.py
+++ battery/NubbinBattery.py
@@ -1,18 +1,15 @@
-from abc import ABC
-
-from car import Battery
-
-from datetime import datetime 
+from battery.battery import Battery
+from utils import add_years_to_date
 
 
-class NubbinBattery(Battery, ABC): 
+class NubbinBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date()
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
             return False
