2880636b1c1c1b172e8c4c2a25117a6ec24aa72c
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-10 14:35:24 -0700

    Update Carrigan.py

--- tire/Carrigan.py
+++ tire/Carrigan.py
@@ -1,1 +1,14 @@
+from tire.tire import Tire
+from utils import add_years_to_date
+from array import *
 
+
+class Carrigan(Tire):
+    def __init__(self, arr):
+        self.arr = arr
+
+    def needs_service(self,arr):
+        for i in arr
+          if i >= 0.9:
+            return True
+        return False
