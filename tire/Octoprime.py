94bacd6f451f5441f8c22958941fc0c7ec48e178
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-10 14:41:17 -0700

    Update Octoprime.py

--- tire/Octoprime.py
+++ tire/Octoprime.py
@@ -1,1 +1,16 @@
+from tire.tire import Tire
+from utils import add_years_to_date
+from array import *
 
+
+class Octoprime(Tire):
+    def __init__(self, arr):
+        self.arr = arr
+
+    def needs_service(self,arr):
+        sum = 0
+        for i in arr
+          sum += i
+        if sum >= 3
+          return True
+        return False
