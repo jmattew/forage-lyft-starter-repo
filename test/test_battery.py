9eeb3ef7dd513f667a58aa6d19e602ae9db73ad0
jmattew <51306296+jmattew@users.noreply.github.com>
2022-08-10 14:03:54 -0700

    Update test_battery.py

--- test/test_battery.py
+++ test/test_battery.py
@@ -7,7 +7,7 @@ from battery.NubbinBattery import NubbinBattery
 class TestSpindler(unittest.TestCase)
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
 
         spBattery = SpindlerBattery(self,today,last_service_date)
         self.assertTrue(spBattery.needs_service())
